32d0006e408e53cd8624a870b9ba23fbbfe6ba1d
Erli Junior <erlijunior@outlook.com>
2025-11-18 11:30:02 -0300

    criado userContext

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,19 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Footer from './Components/Footer'
 import Home from './Components/Home'
 import Login from './Components/Login/Login'
-
+import { UserStorage } from './Contexts/UserContext'
 function App() {
 	return (
 		<div>
 			<BrowserRouter>
-				<Header />
-				<Routes>
-					<Route path='/' element={<Home />} end />
-					<Route path='/login/*' element={<Login />} />
-				</Routes>
-				<Footer />
+				<UserStorage>
+					<Header />
+					<Routes>
+						<Route path='/' element={<Home />} end />
+						<Route path='/login/*' element={<Login />} />
+					</Routes>
+					<Footer />
+				</UserStorage>
 			</BrowserRouter>
 		</div>
 	)
