8e4a1452508280288647e7a09dcb21ecd33dbade
Erli Junior <erlijunior@outlook.com>
2025-11-17 22:52:34 -0300

    adiciona estilos do header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,19 @@
 import { Link } from 'react-router-dom'
 import styles from './Header.module.css'
+import Dog from '../Assets/dogs.svg?react'
+
 const Header = () => {
 	return (
-		<div className={styles.header}>
-			<nav className='container'>
-				<Link to='/'>Home</Link>
-				<Link to='/login'>Login / Criar</Link>
+		<header className={styles.header}>
+			<nav className={`container ${styles.nav}`}>
+				<Link className={styles.logo} to='/' aria-label='Dogs - Home'>
+					<Dog />
+				</Link>
+				<Link className={styles.login} to='/login'>
+					Login / Criar
+				</Link>
 			</nav>
-		</div>
+		</header>
 	)
 }
 export default Header
