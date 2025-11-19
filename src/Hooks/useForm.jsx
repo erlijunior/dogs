e577630320a59b902af100c56c3631b7538d990b
Erli Junior <erlijunior@outlook.com>
2025-11-19 11:57:55 -0300

    feat: implementa cadastro de usuário

--- src/Hooks/useForm.jsx
+++ src/Hooks/useForm.jsx
@@ -6,6 +6,11 @@ const types = {
 			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
 		message: 'Preencha um email válido',
 	},
+	password: {
+		regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/,
+		message:
+			'A senha deve conter ao menos um dígito, uma letra minúscula, uma letra maiúscula, menos um caractere especial, menos 8 dos caracteres mencionados',
+	},
 }
 
 const useForm = (type) => {
