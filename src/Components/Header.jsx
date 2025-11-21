d829f9458d543adc02d07354b30bf260b1160ca2
Erli Junior <erlijunior@outlook.com>
2025-11-21 01:02:44 -0300

    feat: implementa navegação responsiva com botão de menu e estilos para mobile

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ import Dog from '../Assets/dogs.svg?react'
 import { UserContext } from '../Contexts/UserContext'
 
 const Header = () => {
-	const { data, userLogout } = React.useContext(UserContext)
+	const { data } = React.useContext(UserContext)
 
 	return (
 		<header className={styles.header}>
@@ -18,7 +18,6 @@ const Header = () => {
 						<Link className={styles.login} to='/conta'>
 							{data.nome}
 						</Link>
-						<button onClick={userLogout}>Sair</button>
 					</>
 				) : (
 					<Link className={styles.login} to='/login'>
