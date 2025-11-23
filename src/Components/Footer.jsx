7557d9b0e34c34dcbde95cc44fa9b21f87d74c39
Erli Junior <erlijunior@outlook.com>
2025-11-23 18:37:36 -0300

    feat: adiciona estrutura e estilo ao componente Footer e ajusta layout da aplicação

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,11 @@
 import styles from './Footer.module.css'
+import Dogs from '../Assets/dogs-footer.svg?react'
 const Footer = () => {
-	return <footer className={styles.footer}></footer>
+	return (
+		<footer className={styles.footer}>
+			<Dogs />
+			<p>Dogs. Alguns direitos reservados.</p>
+		</footer>
+	)
 }
 export default Footer
