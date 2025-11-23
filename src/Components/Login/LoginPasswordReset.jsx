5088c1112469dd234f3fa2a97863b6cd89cfca3e
Erli Junior <erlijunior@outlook.com>
2025-11-23 19:42:02 -0300

    feat: adiciona meta tags e ícone ao HTML, ajusta estilos de login e adiciona responsividade ao layout

--- src/Components/Login/LoginPasswordReset.jsx
+++ src/Components/Login/LoginPasswordReset.jsx
@@ -33,7 +33,7 @@ const LoginPasswordReset = () => {
 	}, [])
 
 	return (
-		<div>
+		<section className='animeLeft'>
 			<Head title='Resete a senha' />
 			<h1 className='title'>Resete a senha</h1>
 
@@ -42,7 +42,7 @@ const LoginPasswordReset = () => {
 				{loading ? <Button disabled>Resetando...</Button> : <Button>Resetar</Button>}
 			</form>
 			<Error error={error} />
-		</div>
+		</section>
 	)
 }
 export default LoginPasswordReset
