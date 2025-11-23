de638ef596efa1e15bdadb7f690b6167f63d36fc
Erli Junior <erlijunior@outlook.com>
2025-11-23 19:24:55 -0300

    feat: adiciona funcionalidade de estatísticas do usuário com gráficos
    - Adicionada biblioteca Victory.js para capacidades de geração de gráficos.
    - Implementado componente UserStats para buscar e exibir estatísticas do usuário.
    - Criado componente UserStatsGraphs para visualizar dados usando VictoryPie e VictoryBar.
    - Aprimorada estilização para gráficos e layout em UserStatsGraphs.module.css.
    - Atualizada API para incluir função STATS_GET para buscar estatísticas.
    - Adicionado tratamento de carregamento e erro no componente UserStats.

--- src/User/UserStats.jsx
+++ src/User/UserStats.jsx
@@ -1,11 +1,32 @@
+import React from 'react'
 import Head from '../Components/Helper/Head'
+import Loading from '../Components/Helper/Loading'
+import Error from '../Components/Helper/Error'
+import useFetch from '../Hooks/useFetch'
+import { STATS_GET } from '../api'
+import UserStatsGraphs from './UserStatsGraphs'
 
 const UserStats = () => {
-	return (
-		<div>
-			<Head title='Estatísticas' />
-			UserStats
-		</div>
-	)
+	const { data, error, loading, request } = useFetch()
+
+	React.useEffect(() => {
+		async function getData() {
+			const token = window.localStorage.getItem('token')
+			const { url, options } = STATS_GET(token)
+			await request(url, options)
+		}
+		getData()
+	}, [request])
+
+	if (loading) return <Loading />
+	if (error) return <Error error={error} />
+	if (data)
+		return (
+			<div>
+				<Head title='Estatísticas' />
+				<UserStatsGraphs data={data} />
+			</div>
+		)
+	else return null
 }
 export default UserStats
---
+++ src/User/UserStatsGraphs.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import styles from './UserStatsGraphs.module.css'
+import { VictoryPie, VictoryBar, VictoryChart } from 'victory'
+
+const UserStatsGraphs = ({ data }) => {
+	const [graph, setGraph] = React.useState([])
+	const [total, setTotal] = React.useState(0)
+
+	React.useEffect(() => {
+		const graphData = data.map((item) => {
+			return {
+				x: item.title,
+				y: +item.acessos,
+			}
+		})
+		setTotal(data.reduce((acc, cur) => acc + +cur.acessos, 0))
+		setGraph(graphData)
+	}, [data])
+	return (
+		<section className={`${styles.graph} animeLeft`}>
+			<div className={`${styles.graphItem} ${styles.total}`}>
+				<p>Acessos: {total}</p>
+			</div>
+			<div className={`${styles.graphItem}`}>
+				<VictoryPie
+					data={graph}
+					innerRadius={50}
+					padding={{ top: 20, bottom: 30, left: 80, right: 80 }}
+					style={{
+						data: {
+							fillOpacity: 0.9,
+							stroke: '#fff',
+							strokeWidth: 2,
+						},
+						labels: {
+							fontSize: 14,
+							fill: '#333',
+						},
+					}}
+				/>
+			</div>
+			<div className={`${styles.graphItem}`}>
+				<VictoryChart padding={{ top: 20, bottom: 20, left: 80, right: 80 }}>
+					<VictoryBar alignment='start' data={graph}></VictoryBar>
+				</VictoryChart>
+			</div>
+		</section>
+	)
+}
+export default UserStatsGraphs
