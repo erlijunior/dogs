fc507599c635cadb598eb5ccd550bb79c6bd34ed
Erli Junior <erlijunior@outlook.com>
2025-11-23 13:52:35 -0300

    feat: adiciona componente NotFound e implementa rotas para tratamento de páginas não encontradas

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import { UserStorage } from './Contexts/UserContext'
 import User from './User/User'
 import ProtectedRoute from './Components/Helper/ProtectedRoute'
 import UserProfile from './User/UserProfile'
+import NotFound from './Components/NotFound'
 function App() {
 	return (
 		<div>
@@ -29,6 +30,7 @@ function App() {
 						/>
 						<Route path='foto/:id' element={<Photo />} />
 						<Route path='perfil/:user' element={<UserProfile />} />
+						<Route path='*' element={<NotFound />} />
 					</Routes>
 					<Footer />
 				</UserStorage>
--- src/User/User.jsx
+++ src/User/User.jsx
@@ -5,6 +5,7 @@ import UserPhotoPost from './UserPhotoPost'
 import UserStats from './UserStats'
 import React from 'react'
 import { UserContext } from '../Contexts/UserContext'
+import NotFound from '../Components/NotFound'
 
 const User = () => {
 	const { data } = React.useContext(UserContext)
@@ -15,6 +16,7 @@ const User = () => {
 				<Route path='/' element={<Feed user={data.id} />} />
 				<Route path='postar' element={<UserPhotoPost />} />
 				<Route path='estatisticas' element={<UserStats />} />
+				<Route path='*' element={<NotFound />} />
 			</Routes>
 		</section>
 	)
